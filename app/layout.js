5a52c497cbb7b9168529ffab0a169047556b7c5b
Habibur Rahman <amarhabibur@gmail.com>
2024-11-24 22:34:42 +0600

    ..

--- app/layout.js
+++ app/layout.js
@@ -32,7 +32,7 @@ export default async function RootLayout({ children }) {
           <div className="relative z-">{children}</div>
 
           {/* Add another div for gradient background with a lower z-index, but still above the parent background */}
-          <div className="absolute inset-0 bg-[linear-gradient(to_right,#0d9488_1px,transparent_1px),linear-gradient(to_bottom,#0d9488_3px,transparent_3px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_70%_at_60%_40%,#000_70%,transparent_110%)] opacity-10 z-0" />
+          <div className="absolute inset-0 bg-[linear-gradient(to_right,#0d9488_1px,transparent_1px),linear-gradient(to_bottom,#0d9488_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_70%_at_60%_40%,#000_70%,transparent_110%)] opacity-10 z-0" />
         </div>
       </body>
     </html>
